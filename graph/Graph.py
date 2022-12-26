9c4db4842e57c7c55e0fad67b4b882ae5f2abf16
Angela Wang <wangela472@gmail.com>
2022-12-25 22:28:42 -0500

    initial commit with graph data and classes

---
+++ graph/Graph.py
@@ -0,0 +1,41 @@
+class Node:
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
+
+    def __repr__(self) -> str:
+        return repr(vars(self))
+
+    def get_pos(self):
+        return self.longitude, self.latitude
+
+
+class Edge:
+    def __init__(self, node1: str, node2: str, **kwargs):
+        self.node1 = node1
+        self.node2 = node2
+        self.__dict__.update(kwargs)
+
+    def __repr__(self) -> str:
+        return repr(vars(self))
+
+
+class Graph:
+    def __init__(self, nodes: dict[str, Node], edges: list[Edge]):
+        self.nodes = nodes
+        self.edges = edges
+        self.adj = {}
+
+        self.__add_nodes_to_adj()
+        self.__add_edges_to_adj()
+
+    def __add_nodes_to_adj(self):
+        for node in self.nodes:
+            self.adj[node] = []
+
+    def __add_edges_to_adj(self):
+        for edge in self.edges:
+            self.adj[edge.node1].append(edge.node2)
+            self.adj[edge.node2].append(edge.node1)
+
+    def __repr__(self):
+        return repr(vars(self))
---
+++ graph/GraphBuilder.py
@@ -0,0 +1,61 @@
+from abc import ABC, abstractmethod
+from typing import Tuple
+from .Graph import Node, Edge
+import csv
+
+
+class iGraphBuilder(ABC):
+    def __init__(self):
+        self.nodes = {}
+        self.edges = []
+
+    @abstractmethod
+    def build_components(self) -> Tuple[dict[str, Node], list[Edge]]:
+        return self.nodes, self.edges
+
+
+class LondonGraphBuilder(iGraphBuilder):
+    def __init__(
+        self, stations_file: str, connections_file: str, lines_file: str
+    ):
+        self.connections_file = connections_file
+        self.lines_file = lines_file
+        self.stations_file = stations_file
+        return super().__init__()
+
+    def __read_station_data(self):
+
+        # open stations file to get station (node) data
+        with open(self.stations_file) as csvfile:
+            stations = csv.DictReader(csvfile)
+
+            # loop through each station and add as node to graph
+            for station in stations:
+                station["id"] = int(station["id"])
+                station["latitude"] = float(station["latitude"])
+                station["longitude"] = float(station["longitude"])
+                node = Node(**station)
+                self.nodes[station["id"]] = node
+
+    def __read_connection_data(self):
+
+        # open connections file to get connection (edge) data
+        with open(self.connections_file) as file:
+            connections = csv.DictReader(file)
+
+            # loop through each connection and add as edge to graph
+            for connection in connections:
+                station1_id, station2_id = int(
+                    connection.pop("station1")
+                ), int(connection.pop("station2"))
+                connection["line"] = int(connection["line"])
+                connection["time"] = int(connection["time"])
+
+                edge = Edge(station1_id, station2_id, **connection)
+                self.edges.append(edge)
+
+    def build_components(self) -> Tuple[dict[str, Node], list[Edge]]:
+        self.__read_station_data()
+        self.__read_connection_data()
+
+        return super().build_components()
